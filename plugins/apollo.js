b54316015400b7a4daaffcdcafc1ca727d1cb308
Nolan Darilek <nolan@thewordnerd.info>
2017-03-29 15:02:50 -0500

    Only initialize VueApollo on the client, as SSR is 1) probably not crucial for Sandstorm and 2) causing lots of Vue inconsistency.

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -2,19 +2,21 @@ import ApolloClient, {createNetworkInterface} from "apollo-client"
 import Vue from "vue"
 import VueApollo from "vue-apollo"
 
-const uri = process.BROWSER_BUILD ? "/api" : "http://127.0.0.1:8000/api"
+if(process.BROWSER_BUILD) {
+  const uri = "/api"
 
-const networkInterface = createNetworkInterface({
-  uri,
-  opts: {
-    credentials: "same-origin"
-  }
-})
+  const networkInterface = createNetworkInterface({
+    uri,
+    opts: {
+      credentials: "same-origin"
+    }
+  })
 
-const apolloClient = new ApolloClient({
-  networkInterface
-})
+  const apolloClient = new ApolloClient({
+    networkInterface
+  })
 
-Vue.use(VueApollo, {
-  apolloClient
-})
+  Vue.use(VueApollo, {
+    apolloClient
+  })
+}
